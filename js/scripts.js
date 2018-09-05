dbc3a577bdba22d77ffc23c6270ed680d65f72ef
Wanda Kalinowska <wanda0501@gmail.com>
2018-09-05 18:30:01 +0200

    Add script

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var url = 'http://api.icndb.com/jokes/random';
+getJoke(); // ->Wygeneruje nam żart przy wejśćiu na stronę
+
+var button = document.getElementById('get-joke');
+button.addEventListener('click', function(){
+  getJoke();
+});
+
+var paragraph = document.getElementById('joke');
+
+function getJoke() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.addEventListener('load', function(){
+    var response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
+
+
